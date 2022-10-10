fc372724a931ca8c9c575e028313685ab033d088
Luis Neira <luis.neira@lossexpress.com>
2022-10-10 18:59:11 -0400

    Initial setup

---
+++ mocks/task_types.js
@@ -0,0 +1,143 @@
+
+module.exports = [
+  {
+    id: 'a4f26b03-a3aa-493a-9a69-7f3badfcb902',
+    name:'Send Loss Initiation Docs',
+    dueBy: '8 hours'
+  },
+  {
+    id: 'c7238a27-0f87-4244-a792-f07ddcec0fd2', 
+    name: 'Loss Initiation Call',
+    dueBy:  '1 hour'
+  },
+  {
+    id: 'a6c08bbb-312e-4d9a-98e8-79d26631cda9', 
+    name: 'Fulfill Order(s)',
+    dueBy:	'1 hour'
+  },
+  {
+    id: 'ff466f81-aa79-4c4a-88c9-24aac24fffcd', 
+    name: 'Review for Changes', 
+    dueBy:	'1 hours'
+  },
+  {
+    id: '804cc953-0e0e-4960-a9ab-b8a7c03e8fe6', 
+    name: 'Follow-up on Check', 
+    dueBy:	'4 hours'
+  },
+  {
+    id: '66c1f7e3-ebce-4203-9a5c-96b22ff492a1', 
+    name: 'Create Document', 
+    dueBy:	'30 minutes'
+  },
+  {
+    id: '2c80eee0-fb81-49c8-aef9-046b4cff102a', 
+    name: 'Request Customer Authorization', 
+    dueBy:	'1 hours'
+  },
+  {
+    id: 'd56ddf90-10fb-44e2-9cd8-83107e002ad6', 
+    name: 'Fulfill Order(s)', 
+    dueBy:	'1 hour'
+  },
+  {
+    id: '97b7d98c-e102-4005-8120-f58f36e06329', 
+    name: 'Handle Disputed Settlement Counter', 
+    dueBy:	'4 hours'
+  },
+  {
+    id: 'ee8da20c-9c8a-41d9-8f3c-7b7a32654b2e', 
+    name: 'Follow-up on LoG', 
+    dueBy:	'2 hours'
+  },
+  {
+    id: 'f60e8cfb-5800-498f-9615-86e7653739ae', 
+    name: 'Loss Initiation Call', 
+    dueBy:	'1 hour'
+  },
+  {
+    id: 'c4e3fd63-7945-4f2f-ab9e-6ff303556b2a', 
+    name: 'Call for Payoff', 
+    dueBy:	'45 minutes'
+  },
+  {
+    id: '534abb76-15b8-47b3-8f95-354c085bc1da', 
+    name: 'Payoff Entry', 
+    dueBy: '10 minutes'
+  },
+  {
+    id: '24ecd3da-c447-4146-ad32-964014142e6c', 
+    name: 'Follow-up on Payoff', 
+    dueBy:	'2 hours'
+  },
+  {
+    id: 'a55f1a19-9a45-4ee0-beb2-e9a3d792cf72', 
+    name: 'Review for Quality', 
+    dueBy:	'4 hours'
+  },
+  {
+    id: 'ad841fe2-0f37-48f7-afa1-fc546404ccb2', 
+    name: 'Re-send LoG Request', 
+    dueBy:	'2 hours'
+  },
+  {
+    id: '10bb46bc-269c-4c23-ac28-aa26ddcab6f8', 
+    name: 'Review Document from Adjuster',
+    dueBy:	'15 minutes'
+  },
+  {
+    id: 'aeb239fe-5c74-4ed2-902f-07fac22d8d07', 
+    name: 'Review Note from Lender', 
+    dueBy:	'1 hours'
+  },
+  {
+    id: '53731c3f-552a-4fc2-bdb4-251b00d8eeca', 
+    name: 'Review Lender Counter-Offer	1', 
+    dueBy: 'hours'
+  },
+  {
+    id: '82f68023-80b7-40c1-9782-2312492f7b33', 
+    name: 'Review Lender Offer', 
+    dueBy:	'2 hours'
+  },
+  {
+    id: '80e642ba-31db-47fc-8411-a0f31570f9e0', 
+    name: 'Send Message to Adjuster', 
+    dueBy:	'1 hours'
+  },
+  {
+    id: 'b96f52b6-bcc9-4913-a196-845c5ab4c5cf', 
+    name: 'Send LoG Request', 
+    dueBy:	'2 hours'
+  },
+  {
+    id: '558310b5-4d2d-4c0f-899d-bd1b3af3dab4', 
+    name: 'Send Loss Initiation Docs', 
+    dueBy:	'8 hours'
+  },
+  {
+    id: '63ffb3ca-374a-4e3d-9013-e9d0c45970d9', 
+    name: 'Send Payoff Request', 
+    dueBy: '1 hour'
+  },
+  {
+    id: 'f23de472-b250-49d0-b865-fa6e0e9679e0', 
+    name: 'Follow-up on Title', 
+    dueBy:	'4 hours'
+  },
+  {
+    id: '5f2cad3a-4e14-4a18-94a7-82cf3912c09f', 
+    name: 'Validate Document', 
+    dueBy:	'10 minutes'
+  },
+  {
+    id: '08492ca0-4520-4618-a33c-48a808abbc1d', 
+    name: 'Verify Lender', 
+    dueBy:	'2 hours'
+  },
+  {
+    id: '0c56c248-0dc0-4a4d-8776-ed9f49b8903f', 
+    name: 'Written Payoff (Customer Auth)', 
+    dueBy:	'1 hour'
+  },
+]
---
+++ seeds/local/08_orders.js
@@ -0,0 +1,166 @@
+'use strict';
+
+const faker = require('@faker-js/faker').faker;
+const { v4: uuidv4 } = require('uuid');
+const { add } = require('date-fns');
+
+const orderTypes = require('../../mocks/order-types');
+const taskTypes = require('../../mocks/task_types');
+
+// Adds vendors and requesting companies.
+exports.seed = async knex => {
+  await knex('packets').del()
+  await knex('orders').del();
+  await knex('order_task').del();
+  await knex('tasks').del();
+
+  const packetId = uuidv4();
+  const orderId = uuidv4();
+  let taskId = uuidv4();
+  const orderType = faker.helpers.arrayElement(orderTypes);
+
+  await knex('requesting_companies').insert([
+    {
+      'requesting_company_id': '931cbbba-3ff6-487f-b3b5-76a27c897afc',
+    },
+  ]);
+
+  await knex('vendors').insert([
+    {
+      'vendor_id': 'FL',
+      'vendor_secret': 'SECRET_SECRET',
+    },
+  ]);
+
+  await knex('packets').insert([
+    {
+      'packet_id': 'd26a97bb-951b-4a9c-a421-637f3184bbea',
+      'requesting_company_id': '931cbbba-3ff6-487f-b3b5-76a27c897afc',
+      'vendor_id': 'FL',
+    },
+  ]);
+
+  await knex('packets').insert([
+    {
+      packet_id: packetId,
+      lender_id: '191bc552-230d-4568-af03-7231c4270f5e', // depend
+      lender_name: 'Fastlane Test Bank (Not-Automatic)',
+      vin: '1N4AL3AP8JC231502',
+      claim_number: 'random claim number',
+      account_number: '20351b4f6f1b322a582a29c29ccc6a30$8760a991f6eee2d6d765e1e701906e5c09cbf39bf18ca4da$f91dd28ec538a7287b435c205571176fd0a87ee59534324ac0f65c7e549f9c5c',
+      owners_name: `${faker.name.firstName()} ${faker.name.lastName()}`,
+      owners_street_address: `${faker.random.numeric(2, { allowLeadingZeros: true })} ${faker.address.street()}`,
+      owners_phone_number: faker.phone.number('(###) ###-###'),
+      settlement_amount: faker.commerce.price(20_000, 50_000),
+      deductible: faker.commerce.price(500, 2_000),
+      date_of_loss: '2022-04-03',
+      finance_type: 'Retial',
+      requesting_company_id: '931cbbba-3ff6-487f-b3b5-76a27c897af2',
+      vendor_id: 'LE',
+      created_at: knex.raw('NOW()'),
+      updated_at: knex.raw('NOW()'),
+      make: 'NISSAN',
+      model: 'Altima',
+      year: '2018',
+      userId: '79ba8da5-5b51-47c6-a016-5f98c0e22916',
+      archived: false,
+      title_remittance_address: '95 Stehr Parkway',
+      odometer: 'random number string',
+      tags: '[]',
+      owners_name_updated: false,
+      owners_address_updated: false,
+      internal_status_updated_at: new Date().toJSON(), // todo
+      internal_status: 'Newly Added to Queue',
+      do_not_notify_users: '[]',
+      owner_retained: false,
+      cause_of_loss: 'Single-Vehicle Collision',
+      titled_owners_name_verified: false,
+      review_any_changes: false,
+      received_by_gcm: false,
+      updated_since_gcm: false,
+      insurer_type: 'First Party',
+      titled_state_verified: false,
+      title_document_added: false,
+      assigned_fax_number: '12145010977',
+      lender_fax_number: '+999-9999-0',
+      lender_has_title: false,
+      followup_count: 0,
+      test_claim: true,
+      processing_group: 'lossexpress',
+    },
+  ]);
+
+  await knex('orders').insert([
+    {
+      order_id: orderId,
+      packet_id: packetId,
+      type: orderType.type,
+      status: 'pending',
+      created_at: knex.raw('NOW()'),
+      updated_at: knex.raw('NOW()'),
+      order_type_id: orderType.id,
+      enabled: orderType.enabled,
+      attempt_count: 0,
+      system_attempt_count: 0,
+      oda_failed: false,
+    },
+  ]);
+
+  const loadedTaskTypes = [];
+
+  if (!loadedTaskTypes.includes(orderType.taskTypeId)) {
+    taskId = uuidv4();
+
+    const [ num, int] = taskTypes.find((t) => t.id === orderType.taskTypeId).dueBy.split(' ');
+
+    await knex('tasks').insert([
+      {
+        task_id: taskId,
+        packet_id: packetId, //depend
+        status: 'unstarted',
+        due_by: add(new Date(), {
+          [int]: parseInt(num)
+        }),
+        priority: 1,
+        created_at: knex.raw('NOW()'),
+        updated_at: knex.raw('NOW()'),
+        task_type_id: orderType.taskTypeId,
+        processingGroup: 'lossexpress',
+      },
+    ]);
+  
+    loadedTaskTypes.push(orderTypes.taskTypeId);
+    
+  }
+
+  await knex('order_task').insert([
+    {
+      order_id: orderId,
+      task_id: taskId,
+      created_at: knex.raw('NOW()'),
+      updated_at: knex.raw('NOW()'),
+    },
+  ]);
+
+  await knex('documents').insert([
+    {
+      packet_id: packetId,
+      document_name: `${packetId}-Regular PDF.pdf`,
+      created_at: knex.raw('NOW()'),
+      updated_at: knex.raw('NOW()'),
+      deleted: false,
+      is_form: false,
+      type: 'settlement breakdown'
+    },
+    {
+      packet_id: packetId,
+      document_name: `${packetId}-Regular PDF.pdf`,
+      created_at: knex.raw('NOW()'),
+      updated_at: knex.raw('NOW()'),
+      deleted: false,
+      is_form: false,
+      type: 'valuation report'
+    },
+  ]);
+
+};
