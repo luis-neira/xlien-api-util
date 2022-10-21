513520c4f96287450c8fb368d13e000e262fed02
Luis Neira <luis.neira@lossexpress.com>
2022-10-21 17:34:34 -0400

    Update order-tpes.js and scripts

--- mocks/order-types.js
+++ mocks/order-types.js
@@ -4,54 +4,54 @@ module.exports = [
     orderTypeId: '87b2ea57-56a3-42d6-82d3-14277a1d727d',
     orderType: 'Copy of Title',
     enabled: true,
-    taskTypeId: 'a7bf1174-ef6b-40aa-bf91-4148810efcf0'
+    taskTypeId: 'd56ddf90-10fb-44e2-9cd8-83107e002ad6'
   },
   { 
     orderTypeId: '2c84eaf7-244e-438c-98df-f8b5f1771953',
     orderType: 'Title Status',
     enabled: false,
-    taskTypeId: 'a7bf1174-ef6b-40aa-bf91-4148810efcf0'
+    taskTypeId: 'd56ddf90-10fb-44e2-9cd8-83107e002ad6'
   },
   {
     orderTypeId: '6c8a9e6f-6b3f-40ee-bb55-a98f0afb4175',
     orderType: 'Lien Release Letter',
     enabled: false,
-    taskTypeId: 'a7bf1174-ef6b-40aa-bf91-4148810efcf0'
+    taskTypeId: 'd56ddf90-10fb-44e2-9cd8-83107e002ad6'
   },
   {
     orderTypeId: '9a153f91-729c-4d6d-b577-a6c818959aa4',
     orderType: 'Installment Contract',
     enabled: false,
-    taskTypeId: 'a7bf1174-ef6b-40aa-bf91-4148810efcf0'
+    taskTypeId: 'd56ddf90-10fb-44e2-9cd8-83107e002ad6'
   },
   {
     orderTypeId: '38615d5d-26e0-406f-833a-ac121210772d',
     orderType: 'Bill of Sale',
     enabled: false,
-    taskTypeId: 'a7bf1174-ef6b-40aa-bf91-4148810efcf0'
+    taskTypeId: 'd56ddf90-10fb-44e2-9cd8-83107e002ad6'
   },
   {
     orderTypeId: '958a6906-6231-420a-8f22-a4d04353f6ab',
     orderType: 'Payment History',
     enabled: true,
-    taskTypeId: 'a7bf1174-ef6b-40aa-bf91-4148810efcf0'
+    taskTypeId: 'd56ddf90-10fb-44e2-9cd8-83107e002ad6'
   },
   { 
     orderTypeId: '0d6c4a12-80ef-4c6d-a07f-44e1ce71ab1d',
     orderType: 'Repo Affidavit',
     enabled: false,
-    taskTypeId: 'a7bf1174-ef6b-40aa-bf91-4148810efcf0'
+    taskTypeId: 'd56ddf90-10fb-44e2-9cd8-83107e002ad6'
   },
   {
     orderTypeId: '52b04ced-7b8e-4253-a2c7-07b7973deb08',
     orderType: 'One and the Same Letter', 
     enabled: false,
-    taskTypeId: 'a7bf1174-ef6b-40aa-bf91-4148810efcf0'
+    taskTypeId: 'd56ddf90-10fb-44e2-9cd8-83107e002ad6'
    },
   {
     orderTypeId: '9a18d312-e0a9-4417-baca-12e524ee34a3',
     orderType: 'Payment Status',
     enabled: false,
-    taskTypeId: 'a7bf1174-ef6b-40aa-bf91-4148810efcf0'
+    taskTypeId: 'd56ddf90-10fb-44e2-9cd8-83107e002ad6'
   },
 ];
